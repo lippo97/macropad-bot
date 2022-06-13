7d3c0cc385b8c6ed7acff8af41b843a698cdbf8f
Filippo <filippo.nardini@studio.unibo.it>
2022-06-13 21:26:34 +0200

    Update client to use rabbitpy

--- client/client/main.py
+++ client/client/main.py
@@ -1,8 +1,8 @@
 import sys
-import pika
 import os
 import logging
 import typer
+import rabbitpy
 
 from pynput import keyboard
 from pynput.keyboard import Key
@@ -11,34 +11,34 @@ LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
 logging.basicConfig(level=LOGLEVEL)
 
 def main(hostname: str, port: int):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(hostname, port, heartbeat=0))
-    channel = connection.channel()
-    channel.exchange_declare(exchange='default',
-                             exchange_type='fanout')
-    def send_msg(msg):
-        logging.info(f'Sending message: {msg}')
-        channel.basic_publish(exchange='default',
-                              routing_key='',
-                              body=msg)
+    with rabbitpy.Connection(f'amqp://{hostname}:{port}') as conn:
+        with conn.channel() as channel:
+            default_exchange = rabbitpy.FanoutExchange(channel, 'default')
+            default_exchange.declare()
 
-    def on_press(key):
-        if key == Key.f8:
-            send_msg('command/0')
-        elif key == Key.f9:
-            send_msg('command/1')
-        elif key == Key.f10:
-            send_msg('command/2')
-        elif key == Key.f11:
-            send_msg('command/3')
+            def send_msg(msg):
+                logging.info(f'Sending message: {msg}')
+                message = rabbitpy.Message(channel, msg)
+                message.publish(exchange=default_exchange)
 
-    print('Client listening for keys.')
-    try:
-        with keyboard.Listener(on_press=on_press) as listener:
-            listener.join()
-    except KeyboardInterrupt:
-        print('Ctrl-C detected, attempting to close gracefully... ', end='')
-        connection.close()
-        print('Done.')
+            def on_press(key):
+                if key == Key.f8:
+                    send_msg('command/0')
+                elif key == Key.f9:
+                    send_msg('command/1')
+                elif key == Key.f10:
+                    send_msg('command/2')
+                elif key == Key.f11:
+                    send_msg('command/3')
+
+            print('Client listening for keys.')
+            try:
+                with keyboard.Listener(on_press=on_press) as listener:
+                    listener.join()
+            except KeyboardInterrupt:
+                print('Ctrl-C detected, attempting to close gracefully... ', end='')
+                conn.close()
+                print('Done.')
 
 if __name__ == '__main__':
     typer.run(main)
