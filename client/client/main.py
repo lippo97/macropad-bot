65f6e075b15cef0b4f4c11f64adc2587f2f007b0
Filippo <filippo.nardini@studio.unibo.it>
2022-06-15 11:55:42 +0200

    Update client to use a TOML configuration file

--- client/client/main.py
+++ client/client/main.py
@@ -3,14 +3,38 @@ import os
 import logging
 import typer
 import rabbitpy
-
+import toml
 from pynput import keyboard
-from pynput.keyboard import Key
+from pynput.keyboard import Key, KeyCode
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
 logging.basicConfig(level=LOGLEVEL)
 
+key_strings = {
+    Key.f1: 'f1',
+    Key.f2: 'f2',
+    Key.f3: 'f3',
+    Key.f4: 'f4',
+    Key.f5: 'f5',
+    Key.f6: 'f6',
+    Key.f7: 'f7',
+    Key.f8: 'f8',
+    Key.f9: 'f9',
+    Key.f10: 'f10',
+    Key.f11: 'f11',
+    Key.f12: 'f12',
+}
+
+def get_clip_for_key(key: Key, keybindings: dict[str, str]):
+    if key in key_strings:
+        key_str = key_strings[key]
+        if key_str in keybindings:
+            return keybindings[key_str]
+    return None
+
 def main(hostname: str, port: int):
+    keybindings = toml.load('keys.toml')['keys']
+
     with rabbitpy.Connection(f'amqp://{hostname}:{port}') as conn:
         with conn.channel() as channel:
             default_exchange = rabbitpy.FanoutExchange(channel, 'default')
@@ -22,16 +46,11 @@ def main(hostname: str, port: int):
                 message.publish(exchange=default_exchange)
 
             def on_press(key):
-                if key == Key.f8:
-                    send_msg('command/0')
-                elif key == Key.f9:
-                    send_msg('command/1')
-                elif key == Key.f10:
-                    send_msg('command/2')
-                elif key == Key.f11:
-                    send_msg('command/3')
+                clip = get_clip_for_key(key, keybindings=keybindings)
+                if clip is not None:
+                    send_msg(f'play/{clip}')
 
-            print('Client listening for keys.')
+            print('Client listening for keys...')
             try:
                 with keyboard.Listener(on_press=on_press) as listener:
                     listener.join()
